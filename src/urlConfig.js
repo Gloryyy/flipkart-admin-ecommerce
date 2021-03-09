7a6d095d7a4d4731ccb5a6969861aacc94975a48
gloryyy <majdwarrrak@gmail.com>
2021-03-09 14:13:06 +0100

    base url update

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://flipkart-rest-serve.herokuapp.com";
+const baseUrl = process.env.API || "https://flipkart-rest-serve.herokuapp.com";
 
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
